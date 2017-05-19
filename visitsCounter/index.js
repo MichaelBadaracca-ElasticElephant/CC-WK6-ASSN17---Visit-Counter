549954f241c842bff95be702f1c8c3e8d00a0609
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-05-19 17:39:17 -0600

    Modified page visist counter to be stored in a text file. Count is read and written to the text file. Is messy & needs more comments

--- visitsCounter/index.js
+++ visitsCounter/index.js
@@ -4,25 +4,77 @@ var fs = require('fs');
 //CONFIGURATION
 var port = 8080;
 var app = express();
+var visitsFilename = 'homepagevisits.txt';
 
 var homepageVisitCount = 0;
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
+    //serve up cats homepage
     fs.readFile("./cats.html", function(err,data){
         if(err){
             console.log(err);
+            res.status(500);
+            res.send("500 - Internal Server Error");
         }
         //increment the count for each time the homepage is visited
-        homepageVisitCount++;
+        //read the homepage visits from a text file on server
+        fs.readFile(visitsFilename, function (err, data) {
+            if (err) {
+                console.log(err);
+                //if the file does not exist, then make it and give it a count of 1
+                fs.writeFile(visitsFilename, "1", function (err, data) {
+                    if (err) {
+                        console.log(err);
+                        res.status(500);
+                        res.send("500 - Internal Server Error");
+                    }
+                    //write file and respond
+                    res.status = 200;
+                    res.send();
+                });
+                return;
+            }
+            //If if the text file does exist, then get the data and increment it
+            var visits = data.toString();
+            visits++;
+            //Save the count back to the file
+            fs.writeFile(visitsFilename, visits, function (err, data) {
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                    res.send("500 - Internal Server Error");
+                }
+            });
+        });
+
         res.status = 200;
         res.send(data.toString());
     })
 })
 
 app.get("/count", function (req, res) {
-    res.status(200);
-    res.send("Number of Homepage Visits: " + homepageVisitCount.toString());
+    //Read the visit count from the text file
+    fs.readFile(visitsFilename, function (err, data) {
+        if (err) {
+            console.log(err);
+            //if the file does not exist, then make it and give it a count of 1
+            fs.writeFile(visitsFilename,"1", function (err, data) {
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                    res.send("500 - Internal Server Error");
+                }
+                res.status(200);
+                res.send("Number of Homepage Visits: 1");
+            });
+            return;
+        }
+        var visits = data.toString();
+        res.status(200);
+        //Return count to the user
+        res.send("Number of Homepage Visits: " + visits);
+    });
 });
 
 app.use(function (req, res, next) {
@@ -41,4 +93,4 @@ app.use(function(err,req,res,next){
 
 app.listen(port, function(){
     console.log(`Started server at http://localhost:${port}`);
-});
+}); 
